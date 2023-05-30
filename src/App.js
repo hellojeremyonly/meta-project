5f8ae6fc54ccf8b2e644a28bb53a7ea6825d85bf
Jeremy <hellojeremyonly@gmail.com>
2023-05-30 15:10:25 +0800

    Save work in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react';
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <h1>Homepage</h1>
-
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
